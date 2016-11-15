566f714ca1f42fcae8374cb51165fc0ebd2e9334
Plamen Jetchev <plamen.bodywork@gmail.com>
2016-11-14 22:17:28 -0500

    Added simple_form --bootstrap, items_controller and index view

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,19 @@
+class ItemsController < ApplicationController
+	def index
+		
+	end
+
+	def new
+	  @item = Item.new
+	end
+
+	def create
+	  @item = Item.new(item_params)	
+	end
+
+	private
+
+	def item_params
+	  params.require(:item).permit(:title, :description, :price)
+	end
+end
