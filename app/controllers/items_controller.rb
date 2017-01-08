351172e2c233135f12ea99fc1929421a766fb7be
plamen1982 <plamen.hristov@ymail.com>
2017-01-08 18:59:58 -0500

    Add User Authentication

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,12 +10,12 @@ class ItemsController < ApplicationController
 
 	def new
 	  @item = Item.new
-		@categories = Category.all.map{ |c| [c.name, c.id] }
+		#@categories = Category.all.map{ |c| [c.name, c.id] }
 	end
 
 	def create
 	  @item = Item.new(item_params)
-		@item.category_id = params[:category_id]
+		#@item.category_id = params[:category_id]
 
 	  if @item.save
 	  	redirect_to root_path
